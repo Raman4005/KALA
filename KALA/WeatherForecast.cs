044cea29a81c87bc52d2583f0812064ea5c98bcf
Raman4005 <ramanbhallan668@gmail.com>
2021-08-24 14:54:50 +1200

    Add project files.

---
+++ KALA/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace KALA
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
